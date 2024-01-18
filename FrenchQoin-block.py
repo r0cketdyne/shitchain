9057075bfa8c4cbb79d42af0749b145f3bb06f27
Matthew James Stephenson <stephenson@Matthews-MacBook-Pro.local>
2024-01-18 15:49:52 -0500

    burner reference

--- FrenchQoin-block.py
+++ FrenchQoin-block.py
@@ -20,12 +20,12 @@ class Block:
         return sha.hexdigest()
 
 def create_genesis_block():
-    return Block(0, date.datetime.now(), "Genesis Block", "0")
+    return Block(0, date.datetime.now(), "Genesis Burn 1986", "0")
 
 def next_block(last_block):
     this_index = last_block.index + 1
     this_timestamp = date.datetime.now()
-    this_data = "Hey! I'm block " + str(this_index)
+    this_data = "Bonjour, this is French Q block number" + str(this_index)
     this_hash = last_block.hash
     return Block(this_index, this_timestamp, this_data, this_hash)
 
@@ -38,6 +38,6 @@ for i in range(0, num_of_blocks_to_add):
     block_to_add = next_block(previous_block)
     blockchain.append(block_to_add)
     previous_block = block_to_add
-    print("Block #{} has been added to the blockchain".format(block_to_add.index))
+    print("Burner Block #{} has been added to the Frenchchain".format(block_to_add.index))
     print("Hash: {}\n".format(block_to_add.hash))
 
